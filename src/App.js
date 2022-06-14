5b439f29fb7d96f16114908c238c4088a1e8684b
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-06-15 00:09:38 +0530

    starting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  let handleChange = (event) => {
+    setCounter(event.target.value);
+  }
+
   return (
     <div className="App">
       <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <button data-testid="counter-decrement-button" onClick={()}>+</button>
+      <button data-testid="counter-increment-button" onClick={()}>-</button>
     </div>
   );
 }
